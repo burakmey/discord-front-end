dc36451df511e2c87d6831cd3b33ad920298b9ea
burakmey <burak_meydan-gs@hotmail.com>
2024-02-23 22:37:53 +0300

    New colors added.

--- src/constants/GlobalStyle.js
+++ src/constants/GlobalStyle.js
@@ -6,11 +6,13 @@ const GlobalStyle = createGlobalStyle`
     --header-primary: #f2f3f5;
     --font-primary: #b5bac1;
     --text-muted: #949ba4;
+    --text-normal: #dbe1de;
     --input-text: #dbdee1;
     --brand-experiment: #5c64f4;
     --text-link: #00a8fc;
     --background-primary: #313338;
     --background-primary-blue: #5865f2;
+    --background-modifier-accent: #4e50587a;
     --input-background: #1e1f22;
 }`;
 
