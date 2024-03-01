93aa7736063efd6ed25e8420e3a6e94190a4ecdc
burakmey <burak_meydan-gs@hotmail.com>
2024-03-01 19:42:04 +0300

    Cteated routes. Added AuthProvider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 import GlobalStyle from "./constants/GlobalStyle";
-import { UserProvider } from "./context/UserContext";
 import Login from "./pages/Login";
 import User from "./pages/User";
+import Home from "./pages/Home";
+import { useEffect } from "react";
+import { UserProvider } from "./context/UserContext";
+import { AuthProvider } from "./context/AuthContext";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
-function App() {
-  return (
-    <main>
-      <GlobalStyle />
-      {/* <Login></Login> */}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: (
+      <div>
+        404 Not Found back to home button <Link to="/">Home</Link>
+      </div>
+    ),
+  },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/user",
+    element: (
       <UserProvider>
         <User />
       </UserProvider>
-    </main>
+    ),
+  },
+]);
+
+function App() {
+  console.log("App rendered!");
+
+  useEffect(() => {
+    return () => console.log("App unmounted!");
+  }, []);
+
+  return (
+    <AuthProvider>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </AuthProvider>
   );
 }
 
