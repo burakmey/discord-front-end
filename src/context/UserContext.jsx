cdef31091055d5e48bdd31d89018f4baf7f40551
burakmey <burak_meydan-gs@hotmail.com>
2024-03-01 19:38:55 +0300

    Protected route for user page.

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,12 +1,21 @@
-import { createContext, useContext, useRef } from "react";
+import { createContext, useContext, useEffect, useRef } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+  const { authData } = useAuthContext();
+  const navigate = useNavigate();
+  console.log(authData);
+  useEffect(() => {
+    if (authData === null) {
+      navigate("/login", { replace: true });
+    }
+  }, [navigate, authData]);
   const activeItem = { id: 0, ref: useRef(null) };
   const values = { activeItem };
-
-  return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
+  if (authData !== null) return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
 
 export const useUserContext = () => useContext(UserContext);
