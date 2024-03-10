9b733250f56638efccade67d95709ef36e15cbf7
burakmey <burak_meydan-gs@hotmail.com>
2024-03-10 20:03:36 +0300

    User page modified.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,6 +2,45 @@ import { useEffect } from "react";
 import { FlexRow, FullScreen } from "../components/container/Container.styles";
 import ServersSideBar from "../components/sideBar/ServersSideBar";
 import styled from "styled-components";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import useUserContext from "../hooks/useUserContext";
+import useAuthContext from "../hooks/useAuthContext";
+
+function User() {
+  console.log("User rendered!");
+  const axiosPrivate = useAxiosPrivate();
+  const { authTokens } = useAuthContext();
+  const { userData, setUserData } = useUserContext();
+
+  const refreshUserData = async () => {
+    try {
+      const response = await axiosPrivate.post(process.env.REACT_APP_USER_GET_USER_URL, JSON.stringify({ refreshToken: authTokens.refreshToken }), {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      console.log("User:refreshUserData success!", response.data);
+      setUserData(response.data);
+    } catch (error) {
+      console.log("Refresh token has tried to get new access token!");
+      console.error(error);
+    }
+  };
+
+  return (
+    <FullScreen>
+      <button onClick={() => refreshUserData()}>REFRESH</button>
+      <Background>
+        <FlexRow>
+          <ServersSideBar />
+          <MainSideBar />
+        </FlexRow>
+        <MainContainer />
+      </Background>
+    </FullScreen>
+  );
+}
+
+export default User;
 
 const Background = styled.div`
   display: flex;
@@ -23,25 +62,3 @@ const MainContainer = styled.div`
   height: 100%;
   background-color: rgb(49, 51, 56);
 `;
-
-function User() {
-  console.log("User rendered!");
-
-  useEffect(() => {
-    return () => console.log("User unmounted!");
-  }, []);
-
-  return (
-    <FullScreen>
-      <Background>
-        <FlexRow>
-          <ServersSideBar></ServersSideBar>
-          <MainSideBar></MainSideBar>
-        </FlexRow>
-        <MainContainer></MainContainer>
-      </Background>
-    </FullScreen>
-  );
-}
-
-export default User;
