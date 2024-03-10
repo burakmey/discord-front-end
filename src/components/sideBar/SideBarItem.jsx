8e36d3662a12437be7087773dffb1bad663d7d90
burakmey <burak_meydan-gs@hotmail.com>
2024-03-10 20:05:25 +0300

    SideBarItem modified.

--- src/components/sideBar/SideBarItem.jsx
+++ src/components/sideBar/SideBarItem.jsx
@@ -1,33 +1,6 @@
 import { useEffect, useState } from "react";
-import { useUserContext } from "../../context/UserContext";
+import useUserContext from "../../hooks/useUserContext";
 import styled from "styled-components";
-
-const ItemBlock = styled.div`
-  position: relative;
-  width: 100%;
-  display: flex;
-  margin: 0 0 8px 0;
-  justify-content: center;
-`;
-const Item = styled.div`
-  width: 48px;
-  min-height: 48px;
-  border-radius: ${({ $isActive }) => ($isActive ? "33%" : "50%")};
-  color: var(--text-normal);
-  background-color: ${({ $isActive }) => ($isActive ? "var(--background-primary-blue)" : "var(--background-primary)")};
-  transition: all 0.15s ease-out, color 0.15s ease-out;
-  cursor: pointer;
-`;
-const ItemSelector = styled.span`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 4px;
-  height: ${({ $height }) => $height + "px"};
-  border-radius: 0 4px 4px 0;
-  margin: ${({ $height }) => ($height === 40 ? "4px 0" : "14px 0")};
-  background-color: var(--header-primary);
-`;
 
 function SideBarItem({ id }) {
   console.log(`SideBarItem ${id} rendered!`);
@@ -66,3 +39,30 @@ function SideBarItem({ id }) {
 }
 
 export default SideBarItem;
+
+const ItemBlock = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  margin: 0 0 8px 0;
+  justify-content: center;
+`;
+const Item = styled.div`
+  width: 48px;
+  min-height: 48px;
+  border-radius: ${({ $isActive }) => ($isActive ? "33%" : "50%")};
+  color: var(--text-normal);
+  background-color: ${({ $isActive }) => ($isActive ? "var(--background-primary-blue)" : "var(--background-primary)")};
+  transition: all 0.15s ease-out, color 0.15s ease-out;
+  cursor: pointer;
+`;
+const ItemSelector = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 4px;
+  height: ${({ $height }) => $height + "px"};
+  border-radius: 0 4px 4px 0;
+  margin: ${({ $height }) => ($height === 40 ? "4px 0" : "14px 0")};
+  background-color: var(--header-primary);
+`;
