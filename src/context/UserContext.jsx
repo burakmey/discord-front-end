5a200464b93e439ef5050c6b3a4c2a48be3190f7
burakmey <burak_meydan-gs@hotmail.com>
2024-03-10 20:01:43 +0300

    UserContext modified.

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,21 +1,45 @@
-import { createContext, useContext, useEffect, useRef } from "react";
-import { useAuthContext } from "../context/AuthContext";
+import { createContext, useEffect, useRef, useState } from "react";
+import useAuthContext from "../hooks/useAuthContext";
 import { useNavigate } from "react-router-dom";
+import { axiosPrivate } from "../api/axios";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const { authData } = useAuthContext();
+  const { authTokens, setAuthTokens } = useAuthContext();
+  const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
-  console.log(authData);
-  useEffect(() => {
-    if (authData === null) {
-      navigate("/login", { replace: true });
-    }
-  }, [navigate, authData]);
   const activeItem = { id: 0, ref: useRef(null) };
-  const values = { activeItem };
-  if (authData !== null) return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
+  const values = { userData, setUserData, activeItem };
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+    const getUser = async () => {
+      try {
+        console.log(authTokens);
+        const response = await axiosPrivate.post(process.env.REACT_APP_USER_GET_USER_URL, JSON.stringify({ refreshToken: authTokens.refreshToken }), {
+          signal: controller.signal,
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        });
+        console.log("UserContext:useEffect", response.data);
+        if (isMounted) {
+          setUserData(response.data);
+        }
+      } catch (error) {
+        navigate("/login", { state: { from: "/user" }, replace: true });
+      }
+    };
+    getUser();
+    return () => {
+      isMounted = false;
+      controller.abort();
+      console.log("User unmounted!");
+    };
+  }, [authTokens, setAuthTokens, navigate]);
+
+  if (authTokens !== null) return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
 
-export const useUserContext = () => useContext(UserContext);
+export default UserContext;
